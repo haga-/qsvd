bb0191eeb6afd4d873c71ecfc95f0cf38b225f5b
Erick Moreira <efmoreira@inf.ufpel.edu.br>
2016-10-11 10:27:51 -0300

    Inicio Projeto Android.

---
+++ QuestionarioQSVD/app/src/main/java/com/hut8/questionarioqsvd/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hut8.questionarioqsvd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv = (TextView) findViewById(R.id.hello);
+        tv.setText("Que nao vai da oq??");
+    }
+}
