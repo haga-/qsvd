3cb185acf5c973e1263ea55077237d65cf80aca2
Erick Moreira <efmoreira@inf.ufpel.edu.br>
2016-10-11 11:42:13 -0300

    Lib pra menu, menu no xml e java, radioGroup e xml.

--- QuestionarioQSVD/app/src/main/java/com/hut8/questionarioqsvd/MainActivity.java
+++ QuestionarioQSVD/app/src/main/java/com/hut8/questionarioqsvd/MainActivity.java
@@ -2,15 +2,23 @@ package com.hut8.questionarioqsvd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[] SPINNERLIST = {"Android Material Design", "Material Design Spinner", "Spinner Using Material Library", "Material Spinner Example"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //TextView tv = (TextView) findViewById(R.id.hello);
-        //tv.setText("Que nao vai da oq??");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, SPINNERLIST);
+        MaterialBetterSpinner materialDesignSpinner = (MaterialBetterSpinner)
+                findViewById(R.id.android_material_design_spinner);
+        materialDesignSpinner.setAdapter(arrayAdapter);
     }
 }
