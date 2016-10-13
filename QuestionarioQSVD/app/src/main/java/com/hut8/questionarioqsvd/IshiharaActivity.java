2a9f0fd7d34f3cc9f54887c89c803ff065abdcd8
Erick Moreira <efmoreira@inf.ufpel.edu.br>
2016-10-13 12:47:23 -0300

    AS e gradle atualizados. Bastante coisas do front avançando.

---
+++ QuestionarioQSVD/app/src/main/java/com/hut8/questionarioqsvd/IshiharaActivity.java
@@ -0,0 +1,27 @@
+package com.hut8.questionarioqsvd;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class IshiharaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ishihara);
+
+        Button buttonProximo = (Button) findViewById(R.id.buttonProximoIshihara);
+
+        buttonProximo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(IshiharaActivity.this, AvaliacaoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
--- QuestionarioQSVD/app/src/main/java/com/hut8/questionarioqsvd/MainActivity.java
+++ QuestionarioQSVD/app/src/main/java/com/hut8/questionarioqsvd/MainActivity.java
@@ -1,24 +1,65 @@
 package com.hut8.questionarioqsvd;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.github.channguyen.rsv.RangeSliderView;
 import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
 public class MainActivity extends AppCompatActivity {
 
-    String[] SPINNERLIST = {"Android Material Design", "Material Design Spinner", "Spinner Using Material Library", "Material Spinner Example"};
+    MaterialBetterSpinner materialDesignSpinner;
+    String[] SPINNERLIST;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SPINNERLIST = getResources().getStringArray(R.array.escolaridade);
+
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_dropdown_item_1line, SPINNERLIST);
-        MaterialBetterSpinner materialDesignSpinner = (MaterialBetterSpinner)
+        materialDesignSpinner = (MaterialBetterSpinner)
                 findViewById(R.id.android_material_design_spinner);
         materialDesignSpinner.setAdapter(arrayAdapter);
+        materialDesignSpinner.setUnderlineColor(getResources().getColor(R.color.colorAccent));
+        materialDesignSpinner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Main", "onClick: underline " + materialDesignSpinner.getUnderlineColor());
+                materialDesignSpinner.setUnderlineColor(getResources().getColor(R.color.colorAccent));
+                Log.i("Main", "onClick: underline " + materialDesignSpinner.getUnderlineColor());
+            }
+        });
+
+        Button buttonProximo = (Button) findViewById(R.id.buttonProximo);
+        buttonProximo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, IshiharaActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.i("Main", "onOptionsItemSelected: ");
+
+        materialDesignSpinner.setUnderlineColor(getResources().getColor(R.color.colorAccent));
+
+        return super.onOptionsItemSelected(item);
     }
 }
