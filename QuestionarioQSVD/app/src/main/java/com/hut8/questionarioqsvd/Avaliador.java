636973c4bbc6839640371b4c49535e261b4cba15
Erick Moreira <efmoreira@inf.ufpel.edu.br>
2016-10-13 21:06:29 -0300

    Classe Avaliador e pedido pra inserir todas informações.

---
+++ QuestionarioQSVD/app/src/main/java/com/hut8/questionarioqsvd/Avaliador.java
@@ -0,0 +1,56 @@
+package com.hut8.questionarioqsvd;
+
+
+/**
+ * Created by erick on 13/10/16.
+ */
+
+public class Avaliador {
+    public static final int SEXO_MASCULINO = 1;
+    public static final int SEXO_FEMININO = 2;
+    private String nome, idade, escolaridade;
+    private int experiencia, respostaIshihara, sexo;
+    private int avaliacoes[];
+
+    public Avaliador(String nome, String idade, String escolaridade, int sexo, int experiencia) {
+        this.nome = nome;
+        this.idade = idade;
+        this.escolaridade = escolaridade;
+        this.sexo = sexo;
+        this.experiencia = experiencia;
+        // TODO
+        avaliacoes = new int[30]; // quantas serão??
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public String getEscolaridade() {
+        return escolaridade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public int getExperiencia() {
+        return experiencia;
+    }
+
+    public int getRespostaIshihara() {
+        return respostaIshihara;
+    }
+
+    public void setRespostaIshihara(int respostaIshihara) {
+        this.respostaIshihara = respostaIshihara;
+    }
+
+    public int[] getAvaliacoes() {
+        return avaliacoes;
+    }
+}
--- QuestionarioQSVD/app/src/main/java/com/hut8/questionarioqsvd/MainActivity.java
+++ QuestionarioQSVD/app/src/main/java/com/hut8/questionarioqsvd/MainActivity.java
@@ -1,8 +1,6 @@
 package com.hut8.questionarioqsvd;
 
 import android.content.Intent;
-import android.net.Uri;
-import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,15 +8,21 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
-import com.github.channguyen.rsv.RangeSliderView;
 import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
 public class MainActivity extends AppCompatActivity {
 
     MaterialBetterSpinner materialDesignSpinner;
-    String[] SPINNERLIST;
+    String[] spinnerList;
+    EditText editTextNome, editTextIdade;
+    SeekBar seekBar;
+    RadioGroup radioGroup;
+    Avaliador avaliador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +30,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SPINNERLIST = getResources().getStringArray(R.array.escolaridade);
+        spinnerList = getResources().getStringArray(R.array.escolaridade);
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, SPINNERLIST);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, spinnerList);
         materialDesignSpinner = (MaterialBetterSpinner) findViewById(R.id.android_material_design_spinner);
         materialDesignSpinner.setAdapter(arrayAdapter);
 
@@ -42,10 +46,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        editTextNome = (EditText) findViewById(R.id.editTextNome);
+        editTextIdade = (EditText) findViewById(R.id.editTextIdade);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroupSexo);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+
         Button buttonProximo = (Button) findViewById(R.id.buttonProximo);
         buttonProximo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String nome, idade, escolaridade;
+                int sexo, experiencia;
+
+                if(editTextNome.getText().length() > 0 &&
+                        editTextIdade.getText().length() > 0 &&
+                        materialDesignSpinner.getListSelection() >= 0 &&
+                        radioGroup.getCheckedRadioButtonId() >= 0 &&
+                        seekBar.getProgress() > 0){
+                    escolaridade = spinnerList[materialDesignSpinner.getListSelection()];
+                    sexo = radioGroup.getCheckedRadioButtonId() == R.id.radioButtonF ? Avaliador.SEXO_FEMININO : Avaliador.SEXO_MASCULINO;
+                    avaliador = new Avaliador(editTextNome.getText().toString(), editTextIdade.getText().toString(),escolaridade, sexo, seekBar.getProgress());
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Por favor, preencha todos os dados.", Toast.LENGTH_SHORT).show();
+                }
                 Intent intent = new Intent(MainActivity.this, IshiharaActivity.class);
                 startActivity(intent);
             }
