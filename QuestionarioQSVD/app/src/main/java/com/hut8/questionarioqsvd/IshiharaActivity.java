8b798b50a274f5eeed1d5292a9e99713fe068b3f
Erick Moreira <efmoreira@inf.ufpel.edu.br>
2017-02-24 18:11:32 -0300

    Arquivo faltante no commit anterior, um pouco do backend da tela de Ishihara.

--- QuestionarioQSVD/app/src/main/java/com/hut8/questionarioqsvd/IshiharaActivity.java
+++ QuestionarioQSVD/app/src/main/java/com/hut8/questionarioqsvd/IshiharaActivity.java
@@ -3,9 +3,12 @@ package com.hut8.questionarioqsvd;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatEditText;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class IshiharaActivity extends AppCompatActivity {
 
@@ -14,13 +17,39 @@ public class IshiharaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ishihara);
 
-        Button buttonProximo = (Button) findViewById(R.id.buttonProximoIshihara);
+        final Button buttonProximo = (Button) findViewById(R.id.buttonProximoIshihara);
+        final AppCompatEditText editText = (AppCompatEditText) findViewById(R.id.editTextIshihara);
+        final CheckBox checkBox = (CheckBox) findViewById(R.id.checkBoxIshihara);
+
+        final int numeroNaImagem = 42;
 
         buttonProximo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(IshiharaActivity.this, AvaliacaoActivity.class);
-                startActivity(intent);
+                String texto = editText.getText().toString();
+                if (texto.length() > 0 || checkBox.isChecked()) {
+                    if (!checkBox.isChecked()) {
+                        boolean acertou = Integer.parseInt(texto) == numeroNaImagem;
+                    }
+
+                    Intent intent = new Intent(IshiharaActivity.this, AvaliacaoActivity.class);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), getResources().getString(R.string.validacaoIshihara), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkBox.isChecked()) {
+                    editText.setEnabled(false);
+                }
+                else {
+                    editText.setEnabled(true);
+                }
             }
         });
 
