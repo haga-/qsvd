37ee3452b16019785f5220b964e264a3ae5596d8
Erick Moreira <efmoreira@inf.ufpel.edu.br>
2017-02-24 18:15:11 -0300

    Botão pra tentar ver a opção de salvar os dados.

--- QuestionarioQSVD/app/src/main/java/com/hut8/questionarioqsvd/MainActivity.java
+++ QuestionarioQSVD/app/src/main/java/com/hut8/questionarioqsvd/MainActivity.java
@@ -1,6 +1,10 @@
 package com.hut8.questionarioqsvd;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +18,10 @@ import android.widget.SeekBar;
 import android.widget.Toast;
 
 import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -58,20 +66,61 @@ public class MainActivity extends AppCompatActivity {
                 String nome, idade, escolaridade;
                 int sexo, experiencia;
 
+                Log.d("Main", "onClick: Nome: " + editTextNome.getText().length() + " idade: " + editTextIdade.getText().length() +
+                        " spinner: " + materialDesignSpinner.getText().toString().length() +
+                        " radio: " + radioGroup.getCheckedRadioButtonId() + " seekbar: " + seekBar.getProgress());
+
                 if(editTextNome.getText().length() > 0 &&
                         editTextIdade.getText().length() > 0 &&
-                        materialDesignSpinner.getListSelection() >= 0 &&
+                        materialDesignSpinner.getText().toString().length() >= 0 &&
                         radioGroup.getCheckedRadioButtonId() >= 0 &&
-                        seekBar.getProgress() > 0){
-                    escolaridade = spinnerList[materialDesignSpinner.getListSelection()];
+                        seekBar.getProgress() >= 0){
+                    escolaridade = materialDesignSpinner.getText().toString();
                     sexo = radioGroup.getCheckedRadioButtonId() == R.id.radioButtonF ? Avaliador.SEXO_FEMININO : Avaliador.SEXO_MASCULINO;
                     avaliador = new Avaliador(editTextNome.getText().toString(), editTextIdade.getText().toString(),escolaridade, sexo, seekBar.getProgress());
+                    Intent intent = new Intent(MainActivity.this, IshiharaActivity.class);
+                    startActivity(intent);
                 }
                 else{
                     Toast.makeText(getApplicationContext(), "Por favor, preencha todos os dados.", Toast.LENGTH_SHORT).show();
                 }
-                Intent intent = new Intent(MainActivity.this, IshiharaActivity.class);
-                startActivity(intent);
+            }
+        });
+
+        Button buttonSave = (Button) findViewById(R.id.buttonSave);
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("Salvar dados no sd?")
+                        .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                try {
+                                    File myFile = new File(Environment.getExternalStorageDirectory(), "notice_me.txt");
+                                    myFile.createNewFile();
+                                    // FileOutputStream fOut = new FileOutputStream(myFile);
+                                    FileOutputStream fOut = openFileOutput(myFile.toString(), MODE_APPEND );
+                                    OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut, "UTF-8");
+
+                                    avaliador.writeToJSON(myOutWriter);
+
+                                    myOutWriter.close();
+                                    fOut.close();
+                                    Toast.makeText(MainActivity.this, "Escreveu no SD", Toast.LENGTH_SHORT).show();
+                                }
+                                catch (Exception e) {
+                                    Toast.makeText(MainActivity.this, e.getMessage(),Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        })
+                        .setNegativeButton("Não", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // User cancelled the dialog
+                            }
+                        });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
             }
         });
 
