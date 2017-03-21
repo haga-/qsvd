04a998a895aa279678afcb772993a98ecc5e71a6
Erick Moreira <efmoreira@inf.ufpel.edu.br>
2017-03-21 10:03:51 -0300

    Últimas avaliações.

--- Avaliacoes/script.py
+++ Avaliacoes/script.py
@@ -14,15 +14,18 @@ avaliacoes_95 = json.loads(lines_95)
 
 respostas_corretas_ishihara = [42, 12, 8, 5, 74, 2, 97, 5, 73]
 
+print "avaliacoes_80:"
 for avaliador in avaliacoes_80:
 	print avaliador["nome"] + (" -> Possui algum grau de daltonismo." if respostas_corretas_ishihara != avaliador["respostas_ishihara"] else "")
 	#for avaliacao in avaliador["avaliacoes"]:
 		#print "\t"+avaliacao
-	
+print str(len(avaliacoes_80)) + " avaliadores."
+
+print "\navaliacoes_95:"
 for avaliador in avaliacoes_95:
 	#print avaliador["nome"] + ": "
 	#for avaliacao in avaliador["avaliacoes"]:
 		#print "\t"+avaliacao
 	print avaliador["nome"] + (" -> Possui algum grau de daltonismo." if respostas_corretas_ishihara != avaliador["respostas_ishihara"] else "")
 
-
+print str(len(avaliacoes_95)) + " avaliadores."
